7171b5b423a12d586b77e4f4f899d002f99edf4d
Manuel Oyaregui <manu.oyaregui@gmail.com>
2025-07-01 18:45:57 -0300

    change variable name to avoid built-in name

--- scripts/entities/HealthBillboard.gd
+++ scripts/entities/HealthBillboard.gd
@@ -1,5 +1,5 @@
 extends Label3D
 
 
-func set_health(current: int, max: int):
-	text = "HP: %d / %d" % [current, max]
+func set_health(current_value: int, max_value: int):
+	text = "HP: %d / %d" % [current_value, max_value]
