e2d15075c9273d6073feab62694deb19bd19b6ef
Manuel Oyaregui <manu.oyaregui@gmail.com>
2025-07-02 20:21:07 -0300

    remove simpleCombat Stuff

--- scripts/combat/SimpleCombatConfig.gd
+++
@@ -1,8 +0,0 @@
-class_name SimpleCombatConfig
-extends Resource
-
-@export var player_config: SimpleEntityConfig = SimpleEntityConfig.new()
-@export var enemy_config: SimpleEntityConfig = SimpleEntityConfig.new()
-@export var combat_background: String = "default"
-@export var music_track: String = "combat_default"
-@export var lighting_setting: String = "default"
